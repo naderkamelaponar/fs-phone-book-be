25e64b0849c9f464552375ff61da666f71f00e30
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-06-24 23:36:36 +0200

    feat full operations with db

--- api/v1.js
+++ api/v1.js
@@ -1,12 +1,10 @@
 // بسم الله الرحمن الرحيم
-const express = require('express');
-const phoneBook=require('../models/phone-book');
-const app = express.Router();
-app.get('/persons',(_,res)=>{
-
-const persons=phoneBook.find({})
-persons.then((dbRes)=>{
-    const dbres= dbRes ? dbRes : 'No Data';
-    res.status(200).res.send(dbres)
+const Router =require("express");
+const router = Router();
+const PhoneBook =require('../models/phone-book');
+router.route('/persons').get((req,res,next)=>{
+    PhoneBook.find({}).then(persons=>{
+        return res.send('ss')
+    })
 })
-})
+exports.module=router;
--- index.js
+++ index.js
@@ -3,76 +3,91 @@ const express =require("express");
 const morgan = require("morgan");
 const cors = require('cors');
 const PhoneBook =require('./models/phone-book');
+const router =require('./api/v1')
 const app= express();
 const PORT = process.env.PORT || 3001
-
-
-//app.use(cors());
-
 app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan('tiny'));
 app.get('/api/persons',(_,res)=>{
     
    PhoneBook.find({}).then(persons=>{
-    console.log('res',persons)
+    if (persons) {
     res.status(200).json(persons)
     return
+    }else{
+    res.status(404).end()
+    return 
+    }
    }).catch((err)=>{
-    res.status(400).json({error:"error"});
+    res.status(500).end()
     return;
 })
-
 })
 
-app.post('/api/persons',(req,res)=>{
+app.post('/api/persons',(req,res,next)=>{
     const newPerson=req.body;
     if(!newPerson.name || !newPerson.number ){
         res.status(400).json({error:"name or number missing"});
-        return;
+        return
+        
     }
-    
     const person= new PhoneBook({
         name:newPerson.name,
         number:newPerson.number,
     })
-    person.save().then((dbRes)=>{
-        res.status(201).json(dbRes);
-      return 
+    person.save().then((dbPerson)=>{
+       if(dbPerson){
+        res.status(201).json(dbPerson)
+        return
+       }else{
+        res.status(400).end()
+        return
+        
+       }
     }).catch((err)=>{
-        res.status(400).json({error:"error"});
-        return;
+        next(err)
+        
     })
 
     
 })
-app.get('/api/persons/:id',(req,res)=>{
+app.get('/api/persons/:id',(req,res,next)=>{
     const id=req.params.id?req.params.id:null;
      PhoneBook.findById(id).then((p)=>{
-        if(p.length===0){
-            res.status(404).json({error:"person not found"});
+        if(p){
+            res.status(200).json(p)
             return
         }else{
-            res.status(200).json(p);
-            return    
+            res.status(404).end()
+            return
         }
         
         
-    })
+    }).catch((err)=>{next(err)})
         
     })
-app.delete('/api/persons/:id',(req,res)=>{
-    const pId=req.params.id?Number(req.params.id):null;
-    const person=pId ? phoneBook.find(p=>p.id===pId):null;
-    if(person){
-        phoneBook = phoneBook.filter(p => p.id !== pId)  
-    res.status(204).end()
-    }else{
-        res.status(404).send('Not Found')
-    }
 
-})
-
+    app.delete('/api/persons/:id',(req,res,next)=>{
+        const id=req.params.id?req.params.id:null;
+        PhoneBook.findByIdAndDelete(id).then(p=>{
+            if(p){
+                res.status(204).end()
+            }else{
+                res.status(404).end()
+            }
+        })
+        })
+        app.put('/api/persons/:id',(req,res,next)=>{
+            const id=req.params.id?req.params.id:null;
+            const {name,number}=req.body;
+            const person={name,number};
+            PhoneBook.findByIdAndUpdate(id,person,{new:true})
+            .then(p=>{
+                if(p) res.status(200).json(p)
+                else res.status(404).end()
+            })
+        })
 app.get('/info',(_,res)=>{
     let   msg =`<p>Phonebook has info for ${PhoneBook.length} people</p>`;
     msg +=`<p>${new Date()}</p>`;
