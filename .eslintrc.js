ecc4ebd0f734f0a6707cf969e1124bf019e75c95
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-06-26 20:17:05 +0200

    ex 3.21

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'no-trailing-spaces': 'error',
+        
+        'no-console': 0,
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
